3cca7ed18b239988f1601f978845c6575ccf47e8
Anderson Jorge <anderson.jorge33@gmail.com>
2022-07-08 11:50:36 -0300

    Aula - Lidando com CORS no NextJs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   images: {
     domains: ['https://www.google.com.br'],
   },
+  /* configuração dos CORS */
+  /* !Obs: sempre que fizer alguma alteração nesse arquivo de configuração nextConfig, precisa reiniciar o servido. */
+  headers: async () => {
+    return [
+      {
+        // source - qual api sera liberada
+        source: '/api/:path*',
+        // headers - lista de headers que serão liberados
+        headers: [
+          {key: 'Access-Control-Allow-Origin', value: '*'}
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
