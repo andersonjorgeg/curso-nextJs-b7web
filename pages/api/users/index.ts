75ce74893355b16af9ce4a07c1368fa3f45e46a1
Anderson Jorge <anderson.jorge33@gmail.com>
2022-07-08 12:23:42 -0300

    Aula - Lidando com métodos diferentes - GET, POST, PUT, DELETE

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,8 +1,33 @@
 import { NextApiHandler } from 'next';
 import { Users } from '../../../utils/users';
 
-const handler: NextApiHandler = (req, res) => {
+// pegar todos os usuários
+const handlerGet: NextApiHandler = async (req, res) => {
   res.json(Users);
+}
+
+// inserir novos usuários
+const handlerPost: NextApiHandler = async (req, res) => {
+  res.json({status: true});
+}
+
+const handler: NextApiHandler = (req, res) => {
+  /* 
+    GET - serve para buscar informações
+    POST - serve para criar informações
+    PUT - serve para atualizar informações
+    DELETE - serve para deletar informações
+  */
+  switch (req.method) {
+    case 'GET': 
+      handlerGet(req, res);
+      break;
+    case 'POST':
+      handlerPost(req, res);
+      break;
+    default:
+      res.status(405).json({ message: 'Method not allowed' });
+  }
 }
 
 export default handler;
