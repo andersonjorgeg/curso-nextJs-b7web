9a5a1394d27568839cebc60d4647583ac6c247cc
Anderson Jorge <anderson.jorge33@gmail.com>
2022-07-16 13:24:42 -0300

    Aula - Prisma: Selecionando dados 2

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,22 +1,27 @@
 import { NextApiHandler } from 'next';
 import { Users } from '../../../utils/users';
+import prisma from '../../../libs/prisma';
 
-const handler: NextApiHandler = (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
   // recebe o id do usuário que esta sendo requisitado
   const { id } = req.query;
-  
-  // percorre o array de usuários
-  for (let i in Users) {
-    // se o id do usuário for igual ao id do usuário que está sendo requisitado
-    if (Users[i].id.toString() ===  id ) {
-      // retorna o usuário que está sendo requisitado
-      res.json(Users[i])
-      return;
+
+  const user = await prisma.user.findFirst({
+    where: { 
+      // passar o id que vem da req.query como string para inteiro
+      id: parseInt(id as string),
+      active: true
     }
+  });
+
+  //condição para verificar se o usuário existe
+  if (user) {
+    res.json({ status: true, user});
+    return;
   }
 
   // se não encontrar o usuário que está sendo requisitado, retorna um erro.
-  res.status(404).json({ error: 'Usuário não encontrado!' });
+  res.status(404).json({ error: 'Usuário não existe!' });
 }
 
 export default handler;
